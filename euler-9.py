a20e2152c9ddc835437eaf0e9b3cede8d445644e
Rick Toews <rick.toews@gmail.com>
2017-03-25 22:02:36 -0400

    Add code for problem 9.

--- euler-9.py
+++ euler-9.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python
 
 def gcf(a, b):
-    count = 0
-    while count < 10:
+    while b%a > 0:
         a, b = min(a, b), max(a, b)
-        if b%a == 0:
-            break
         b %= a
-        count += 1
-
     return a
 
 
@@ -20,15 +15,23 @@ def pythagorean_triple(a):
             b = int(b_sq**.5)
             if True or gcf(a, b) == 1:
                 c = int((a**2 + b_sq) **.5)
-                results = [b, a, c]
+                triple = [a, b, c]
+                triple.sort()
+                if not triple in results:
+                    results.append(triple)
 
     return results
 
-for i in range(3, 5000):
-    pt = pythagorean_triple(i)
-    if len(pt) > 0:
-        total = reduce(lambda x, y: x+y, pt)
-        print pt, total
-        if total == 1000:
-            print "Found target"
-            break
+processed = []
+for i in range(1, 500):
+    triples = pythagorean_triple(i)
+    if len(triples) > 0:
+        for pt in triples:
+            if not pt in processed:
+                processed.append(pt)
+                total = reduce(lambda x, y: x+y, pt)
+#                if total > 950 and total < 1050:
+                print pt, total
+                if total == 1000:
+                    print "Found target"
+                    break
