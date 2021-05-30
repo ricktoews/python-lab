031b2d06b6ba1ee1262a03899c75d406c63f6f1a
Rick Toews <rick.toews@gmail.com>
2021-05-29 23:20:40 -0700

    Adjust app.py to output Content-type: application/json.

--- app.py
+++ app.py
@@ -8,19 +8,25 @@ from flask import Flask, jsonify
 
 from phi import phi_powers
 
+# OUTPUT application/json
+def format_payload(data):
+	str = json.dumps(data, indent=4, separators=(',', ': '))
+	return str, 200, { 'Content-type': 'application/json' }
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello World!"
+	return "Hello World!"
 
 @app.route('/pythag', methods=['GET'])
 def pythag():
-  return json.dumps(get_pythag(), indent=4, separators=(',', ': '))
+	return format_payload(get_pythag())
 
 @app.route('/phi', defaults={'power': 4})
 @app.route('/phi/<power>', methods=['GET'])
 def phi(power):
-  power = int(power)
-  return json.dumps(phi_powers(power), indent=4, separators=(',', ': '))
+	power = int(power)
+	#return json.dumps(phi_powers(power), indent=4, separators=(',', ': ')), 200, { 'Content-type': 'application/json'}
+	return format_payload(phi_powers(power))
 
